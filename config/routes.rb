cea40b6afc6217fde7edc33495118643da18ba50
neilburt97 <neilburt97@gmail.com>
2017-06-06 13:57:15 +0100

    Add a Basic controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'basic/home'
+
+  get 'basic/help'
+
+  get 'basic/about'
+
+  get 'basic/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
